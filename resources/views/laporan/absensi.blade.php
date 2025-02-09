d7f74eeab161658d5c509fa137621e04d71d1424
Nathaniel Kevin Kurniawan <kevinkurniawan901@gmail.com>
2025-02-09 12:40:14 +0700

    perbaikan laporan absensi

--- resources/views/laporan/absensi.blade.php
+++ resources/views/laporan/absensi.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div class="container">
     <h1 class="my-4 text-center">Laporan Absensi</h1>
+    
     <div class="table-responsive">
         <table class="table table-striped table-bordered">
             <thead class="thead-dark">
@@ -10,12 +11,12 @@
                     <th>ID</th>
                     <th>ID Karyawan</th>
                     <th>Nama Karyawan</th>
-                    <th>Email</th>
-                    <th>Foto</th>
-                    <th>Latitude</th>
-                    <th>Longitude</th>
+                    <th>Status</th>
+                    <th>Waktu Masuk</th>
+                    <th>Waktu Pulang</th>
                     <th>Tanggal</th>
-                    <th>Waktu</th>
+                    <th>Denda</th>
+                    <th>Bonus</th>
                 </tr>
             </thead>
             <tbody>
@@ -24,12 +25,18 @@
                         <td>{{ $item->id }}</td>
                         <td>{{ $item->id_karyawan }}</td>
                         <td>{{ $item->karyawan->nama_karyawan }}</td>
-                        <td>{{ $item->email }}</td>
-                        <td><img src="{{ asset('storage/' . $item->foto) }}" alt="Foto" width="50"></td>
-                        <td>{{ $item->latitude }}</td>
-                        <td>{{ $item->longitude }}</td>
-                        <td>{{ $item->created_at->format('d-m-Y') }}</td>
-                        <td>{{ $item->created_at->format('H:i') }}</td>
+                        <td>{{ ucfirst($item->status) }}</td>
+                        <td>
+                            {{ $item->waktu_masuk ? \Carbon\Carbon::parse($item->waktu_masuk)->format('d-m-Y H:i') : '-' }}
+                        </td>
+                        <td>
+                            {{ $item->waktu_pulang ? \Carbon\Carbon::parse($item->waktu_pulang)->format('d-m-Y H:i') : '-' }}
+                        </td>
+                        <td>
+                            {{ $item->tanggal ? \Carbon\Carbon::parse($item->tanggal)->format('d-m-Y') : '-' }}
+                        </td>
+                        <td>{{ number_format($item->denda ?? 0, 0, ',', '.') }}</td>
+                        <td>{{ number_format($item->bonus ?? 0, 0, ',', '.') }}</td>
                     </tr>
                 @endforeach
             </tbody>
